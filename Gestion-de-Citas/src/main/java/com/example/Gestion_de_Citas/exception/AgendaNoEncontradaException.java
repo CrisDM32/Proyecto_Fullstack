ed5f6a06d94b1d889b3b1b6cdfdf74c78d7cf894
CrisDM32 <cristiandm32@gmail.com>
2026-06-12 20:14:53 -0400

    Delete Gestion-de-Citas directory

--- Gestion-de-Citas/src/main/java/com/example/Gestion_de_Citas/exception/AgendaNoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Gestion_de_Citas.exception;
-
-public class AgendaNoEncontradaException extends RuntimeException {
-    public AgendaNoEncontradaException(Long id) {
-        super("No se ha encontrado la agenda medica con el id:" + id);
-    }
-}
--- Gestion-de-Citas/src/main/java/com/example/Gestion_de_Citas/exception/CitaNoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Gestion_de_Citas.exception;
-
-public class CitaNoEncontradaException extends RuntimeException {
-    public CitaNoEncontradaException(Long id) {
-        super("No se ha encontrado la cita con el id: "+ id);
-    }
-}
