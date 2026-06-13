f60abf8f437215cfc5a5eff541825476b2a893bc
CrisDM32 <cristiandm32@gmail.com>
2026-06-12 20:15:08 -0400

    Delete Pagos directory

--- Pagos/src/main/java/com/example/Pagos/exception/CitaNoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Pagos.exception;
-
-public class CitaNoEncontradaException extends RuntimeException {
-    public CitaNoEncontradaException(Long id) {
-        super("No fue encontrada la Cita con el id:" + id);
-    }
-}
--- Pagos/src/main/java/com/example/Pagos/exception/PagoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Pagos.exception;
-
-public class PagoNoEncontradoException extends RuntimeException {
-    public PagoNoEncontradoException(Long id) {
-        super("No se ha encontrado el pago con id: "+ id);
-    }
-}
