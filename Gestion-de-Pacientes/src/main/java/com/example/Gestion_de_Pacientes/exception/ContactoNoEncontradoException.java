bebfc7890d43b45cac4b16dc4a7a72ee6b2d4b4f
CrisDM32 <cristiandm32@gmail.com>
2026-06-12 20:15:01 -0400

    Delete Gestion-de-Pacientes directory

--- Gestion-de-Pacientes/src/main/java/com/example/Gestion_de_Pacientes/exception/ContactoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Gestion_de_Pacientes.exception;
-
-public class ContactoNoEncontradoException extends RuntimeException {
-    public ContactoNoEncontradoException(Long id) {
-        super("No se ha encontrado el contacto de emergencia con el id: "+ id);
-    }
-}
